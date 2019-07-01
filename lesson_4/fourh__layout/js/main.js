d71fd82a4dac65592de18e693e38fa99d4cef03a
Daniil <ldahtessl@gmail.com>
2019-07-01 15:13:40 +0300

    Added all styles

--- lesson_4/fourh__layout/js/main.js
+++ lesson_4/fourh__layout/js/main.js
@@ -2,9 +2,20 @@ $(document).ready(function() {
   $('.header__slider').slick({
     dots: true,
     dotsClass: 'header__slider-dots',
+    prevArrow: '<button class="header__slider-arrow header__slider-arrow_left"></button>',
+    nextArrow: '<button class="header__slider-arrow header__slider-arrow_right"></button>',
     infinite: true,
     speed: 500,
     fade: true,
     cssEase: 'linear'
+  });
+  $('.posts__slider').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button class="posts__slider-arrow posts__slider-arrow_left"></button>',
+    nextArrow: '<button class="posts__slider-arrow posts__slider-arrow_right"></button>',
+    speed: 500
   });
 })
