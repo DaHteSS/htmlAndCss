89370e40cda25189c546ba1de2fa004d534e58ac
Daniil <ldahtessl@gmail.com>
2019-07-03 13:39:03 +0300

    Added media queries (320px - 1230px). Fixed bugs.

--- lesson_4/fourh__layout/js/main.js
+++ lesson_4/fourh__layout/js/main.js
@@ -6,16 +6,53 @@ $(document).ready(function() {
     nextArrow: '<button class="header__slider-arrow header__slider-arrow_right"></button>',
     infinite: true,
     speed: 500,
+    autoplay: true,
+    autoplaySpeed: 5000,
     fade: true,
-    cssEase: 'linear'
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
   });
   $('.posts__slider').slick({
     dots: false,
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    prevArrow: '<button class="posts__slider-arrow posts__slider-arrow_left"></button>',
-    nextArrow: '<button class="posts__slider-arrow posts__slider-arrow_right"></button>',
-    speed: 500
+    prevArrow: '<button class="posts__slider-arrow posts__slider-arrow_right"></button>',
+    nextArrow: '<button class="posts__slider-arrow posts__slider-arrow_left"></button>',
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1245,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  let btn = $(".projects__btn");
+
+  btn.on("click", function(e) {
+    btn.removeClass("projects__btn_active");
+    $(this).addClass("projects__btn_active");
+
+    e.preventDefault();
   });
 })
