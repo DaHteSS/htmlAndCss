c21a3533fe791a08f0ec3bd89b480859c3279ad1
Daniil <ldahtessl@gmail.com>
2019-07-10 23:26:04 +0300

    Added styles for header, slider, timeline, some for team.

---
+++ lesson_5/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 5000
+  });
+});
